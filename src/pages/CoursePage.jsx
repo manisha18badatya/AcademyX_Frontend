855e08a7b93a04285c6caa255697e51dc694a531
Rajeev <bitmasteratj@gmail.com>
2025-05-04 02:27:56 +0530

    UI update

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import VideoContainer from "../components/CoursePage/videocontainer";
-import CourseTracker from "../components/CoursePage/CourseTracker";
+import CourseTracker from "../components/CoursePage/coursetracker";
 import CourseHeading from "../components/CoursePage/CourseHeading";
 import { CourseProvider } from "../context/CourseContext";
+import "../Stylesheets/CoursePage.css";
 
 function CoursePage() {
   return (
     <CourseProvider>
       <Navbar />
-      <div>
-        <CourseHeading />
+      <CourseHeading />
+      <div className="maincontent">
         <VideoContainer />
         <CourseTracker />
       </div>
